17d6b5a222684ca56427c1ab0aa5ec805b3e8ac0
Alessia <alessiatovo@users.noreply.github.com>
2017-08-21 15:52:59 +0200

    Update VoteRequest.java

--- src/VoteRequest.java
+++ src/VoteRequest.java
@@ -1,2 +1,15 @@
-public class VoteRequest {
+import java.io.Serializable;
+
+public class VoteRequest implements Serializable {
+    public final int senderID;
+    public final int currentTerm;
+    public int lastLogTerm;
+    public int lastLogIndex;
+
+    public VoteRequest(int senderID, int currentTerm, int lastLogIndex, int lastLogTerm){
+        this.senderID = senderID;
+        this.currentTerm = currentTerm;
+        this.lastLogIndex = lastLogIndex;
+        this.lastLogTerm = lastLogTerm;
+    }
 }
