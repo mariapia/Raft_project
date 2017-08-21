c296cb9ad66bfbbfc84a264dcca455418116835f
Alessia <alessiatovo@users.noreply.github.com>
2017-08-21 15:40:24 +0200

    Update AppendRequest.java

--- src/AppendRequest.java
+++ src/AppendRequest.java
@@ -1,2 +1,18 @@
-public class AppendRequest {
+import java.io.Serializable;
+import java.util.Map;
+
+public class AppendRequest implements Serializable {
+    public final int senderID;
+    public final int lastLogIndex;
+    public final int nextLogIndex;
+    public final int term;
+    public final Map<Integer, String> logEntry;
+
+    public AppendRequest(int senderID, int lastLogIndex, int nextLogIndex, int term, Map<Integer, String> logEntry){
+        this.senderID = senderID;
+        this.lastLogIndex = lastLogIndex;
+        this.nextLogIndex = nextLogIndex;
+        this.term = term;
+        this.logEntry = logEntry;
+    }
 }
