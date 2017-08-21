47218d8e9f338655ff08bc1e7c72dfe2b6c5e443
Alessia <alessiatovo@users.noreply.github.com>
2017-08-21 15:57:22 +0200

    Update VoteReply.java

--- src/VoteReply.java
+++ src/VoteReply.java
@@ -1,2 +1,14 @@
-public class VoteReply {
+import java.io.Serializable;
+
+public class VoteReply implements Serializable {
+    public final int votedID;
+    public final int term;
+    public final int senderID;
+
+    public VoteReply(int votedID, int term, int senderID){
+        this.votedID = votedID;
+        this.term = term;
+        this. senderID = senderID;
+    }
+
 }
