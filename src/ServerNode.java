fa1487443515eb8cc9afa7c40a1889d981b252da
Alessia <alessia.tovo93@gmail.com>
2017-08-21 16:52:33 +0200

    Updated ServerNode

--- src/ServerNode.java
+++ src/ServerNode.java
@@ -1,9 +1,13 @@
 import akka.actor.Actor;
 import akka.actor.ActorRef;
 import akka.actor.UntypedActor;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
 import java.util.*;
 
 public  class ServerNode extends UntypedActor {
+    Config config = ConfigFactory.load("application");
     protected int id;
     protected List<ActorRef> participants = new ArrayList<ActorRef>();
 
@@ -14,11 +18,24 @@ public  class ServerNode extends UntypedActor {
 
     //variables in Non-Persistent State
     private ServerState state;
+    private int leaderID;
+    private int commitIndex;
+    private Integer [] nextIndex = new Integer[config.getInt("N_SERVER")];
+    private Integer[] matchIndex = new Integer[config.getInt("N_SERVER")];
 
 
     public ServerNode(int id){
         super();
         this.id = id;
+        this.currentTerm = 0;
+        this.leaderID = -1;
+        this.commitIndex = 0;
+        this.state = ServerState.FOLLOWER;
+        for (int i =0; i<config.getInt("N_SERVER"); i++) {
+            nextIndex[i] = 1;
+            matchIndex[i] = 0;
+        }
+
     }
 
     @Override
@@ -28,6 +45,7 @@ public  class ServerNode extends UntypedActor {
             try {
                 for (int i = 0; i < msg.group.size(); i++) {
                     this.participants.add(msg.group.get(i));
+
                 }
 
             } catch (Throwable e) {
@@ -35,12 +53,17 @@ public  class ServerNode extends UntypedActor {
             }
             if (state == ServerState.FOLLOWER){
                 FollowerState.startElection();
-        }
+            }
         }
         System.out.println("Participants.size() "+participants.size()+"  server id "+ id);
+
+
     }
 
 
 
 
 }
+
+
+
